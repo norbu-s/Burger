645c41d4ca4c2217772f4a72eb949fc3e7eb7e08
Norbu Sherpa <norbuts@hotmail.com>
2021-03-14 21:43:26 +1100

    Updated SQL queries and index file

--- config/orm.js
+++ config/orm.js
@@ -1,27 +1,5 @@
 const connection = require('./connection.js');
 
-// const printQuestionMarks = (num) => {
-//   const arr = [];
-
-//   for (let i = 0; i < num; i++) {
-//     arr.push('?');
-//   }
-
-//   return arr.toString();
-// };
-
-// const objToSql = (ob) => {
-//   const arr = [];
-
-//   for (const key in ob) {
-//     let value = ob[key];
-//     if (Object.hasOwnProperty.call(ob, key)) 
-//       arr.push(`${key}=${value}`);
-//   }
-
-//   return arr.toString();
-// };
-
 const orm = {
   all(tableInput, cb) {
     const queryString = `SELECT * FROM ${tableInput};`;
@@ -33,57 +11,35 @@ const orm = {
     });
   },
 
-  create(table, cols, value, cb) {
-    let queryString = `INSERT INTO ${table}`;
+  create(tableName, colName, value, cb) {
+    const queryString = 'INSERT INTO ?? (??) VALUES (?)';
+    connection.query(query, [tableName, colName, value],
+        (err, result) => {
+            if (err) throw err;
+            cb(result);
+        }
+    );
+},
 
-    queryString += ' (';
-    queryString += cols.toString();
-    queryString += ') ';
-    queryString += 'VALUES (';
-    queryString += printQuestionMarks(vals.length);
-    queryString += ') ';
+  update(tableName, colName, colVal, conditionCol, conditionVal, cb) {
+    const query = 'UPDATE ?? SET ?? = ? WHERE ?? = ?';
+        connection.query(query, [tableName, colName, colVal, conditionCol, conditionVal],
+            (err, result) => {
+                if (err) throw err;
+                cb(result);
+            }
+        );
+    },
 
-    console.log(queryString);
-
-    connection.query(queryString, vals, (err, result) => {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  },
-
-  update(table, objColVals, value, cb) {
-    let queryString = `UPDATE ${table}`;
-
-    queryString += ' SET ';
-    queryString += objToSql(objColVals);
-    queryString += ' WHERE ';
-    queryString += condition;
-
-    console.log(queryString);
-    connection.query(queryString, (err, result) => {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  },
-  delete(table, condition, cb) {
-    let queryString = `DELETE FROM ${table}`;
-    queryString += ' WHERE ';
-    queryString += condition;
-
-    connection.query(queryString, (err, result) => {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  },
+  deleteOne(tableName, conditionCol, conditionVal, cb) {
+        const query = 'DELETE FROM ?? WHERE ?? = ?';
+        connection.query(query, [tableName, conditionCol, conditionVal],
+            (err, result) => {
+                if (err) throw err;
+                cb(result);
+            }
+        );
+    }
 };
 
 module.exports = orm;
