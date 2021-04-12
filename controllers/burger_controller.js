90e2c6ab098b6758b4fbfcc865f2c40e6595c8b9
Norbu Sherpa <norbuts@hotmail.com>
2021-04-12 13:39:14 +1000

    Updated controller

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -3,8 +3,8 @@ const burger = require("../models/burger");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    burger.selectAll((data) => {
+router.get("/api/all", (req, res) => {
+    burger.all((data) => {
         const hbsObject = {
             burger: data,
         };
@@ -13,24 +13,40 @@ router.get("/", (req, res) => {
     });
 });
 
-router.post("/add", (req, res) => {
-    burger.createOne(req.body.name, (res) => {
+router.post("/api/add", (req, res) => {
+    burger.create(req.body.name, (result) => {
         res.json({ id: result.insertId });
     });
 });
 
-router.post("/devour/:id", (req, res) => {
-    burger.updateOne([req.body.devoured, req.params.id], condition, (res) => {
-        res.redirect('/');
+router.put("/api/devour/:id", (req, res) => {
+    burger.update([req.body.devoured, req.params.id], condition, (result) => {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        }
+        res.status(200).end();
     });
 });
 
-router.delete("/devour/:id", (req, res) => {
+router.delete("/api/devour/:id", (req, res) => {
     const condition = `id = ${req.params.id}`;
-    burger.deleteOne(condition, (res) => {
-        res.redirect('/');
+
+    burger.delete(condition, (result) => {
+        if (result.affectedRows === 0) {
+            return res.status(404).end();
+        }
+        res.status(200).end();
     });
 });
 
+router.get("/", (req, res) => {
+    burger.all((data) => {
+        const hbsObject = {
+            burger: data,
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
 
 module.exports = router;
