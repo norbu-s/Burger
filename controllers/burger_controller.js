d6f88cb612f31a92ecb28eca5111684201069c74
Norbu Sherpa <norbuts@hotmail.com>
2021-04-12 13:23:18 +1000

    Updated controller to add devour and delete functi

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -3,50 +3,34 @@ const burger = require("../models/burger");
 
 const router = express.Router();
 
-router.get("/api/all", (req, res) => {
-  burger.all((data) => {
-    const hbsObject = {
-      burger: data,
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
-});
-
-router.post("/api/add", (req, res) => {
-  burger.create(req.body.name,(result) => {
-    res.json({ id: result.insertId });
-  });
-});
-
-// router.put("/api/devour/:id", (req, res) => {
-//   burger.update([req.body.devoured, req.params.id], condition, (result) => {
-//     if (result.changedRows === 0) {
-//       return res.status(404).end();
-//     }
-//     res.status(200).end();
-//   });
-// });
-
-router.delete("/api/devour/:id", (req, res) => {
-  const condition = `id = ${req.params.id}`;
-
-  burger.delete(condition, (result) => {
-    if (result.affectedRows === 0) {
-      return res.status(404).end();
-    }
-    res.status(200).end();
-  });
-});
-
 router.get("/", (req, res) => {
-  burger.all((data) => {
-    const hbsObject = {
-      burger: data,
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
+    burger.selectAll((data) => {
+        const hbsObject = {
+            burger: data,
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
 });
+
+router.post("/add", (req, res) => {
+    burger.createOne(req.body.name, (res) => {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.post("/devour/:id", (req, res) => {
+    burger.updateOne([req.body.devoured, req.params.id], condition, (res) => {
+        res.redirect('/');
+    });
+});
+
+router.delete("/devour/:id", (req, res) => {
+    const condition = `id = ${req.params.id}`;
+    burger.deleteOne(condition, (res) => {
+        res.redirect('/');
+    });
+});
+
 
 module.exports = router;
