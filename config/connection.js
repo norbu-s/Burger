5bfcbcb3a7209a8b0b0de3ab0444dc01c6371449
Norbu Sherpa <norbuts@hotmail.com>
2021-04-15 00:05:31 +1000

    Updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "yourRootPassword",
-  database: "burgers_db",
+    host: "localhost",
+    user: "root",
+    password: "yourRootPassword",
+    database: "burgers_db",
 });
 
 connection.connect((err) => {
-  if (err) {
-    console.error(`error connecting: ${err.stack}`);
-    return;
-  }
-  console.log(`connected as id ${connection.threadId}`);
+    if (err) {
+        console.error(`error connecting: ${err.stack}`);
+        return;
+    }
+    console.log(`connected as id ${connection.threadId}`);
 });
-  
+
 module.exports = connection;
